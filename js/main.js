ddbbe4a9bd879d6558dbaddb4a823d176e58371e
Anna Burnett <annairishburnett@gmail.com>
2025-01-16 15:57:51 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,2067 @@
+//Jesse's problem
+//$0.47 for max of range (80%), $0 for min of range (73%), gradient for rest of range
+//difference between min and max is 7
+//gradient from 0 - 7 that aligns with the original array
+//number from previous gradient divided by 7 * $0.47
+
+// function range(min, max, increment = 1){
+//     let array = [];
+//     for(let i = min; i <= max; i += increment){
+//         array.push(Number(i.toFixed(2))); //this isn't working properly - only one decimal, if you take off the Number() constructor function it turns into a string
+//     }
+//     return array
+// }
+
+// console.log(range(73,80,.1));
+
+
+// function calculateBonus(array){
+    
+// }
+
+// console.log(calculateBonus(range(73,80,.1)));
+
+
+
+
+// function sum(array) {
+//     return array.reduce((total, num) => total + num, 0);
+// }
+
+// console.log((sum(range(71,80,.1))));
+
+
+
+
+
+
+
+
+
+
+
+
+// let sentence = ['Loose', 'enough', 'to', 'move', 'freely', 'but', 'tight', 'enough', 'to', 'never', 'let', 'you', 'go'];
+
+// function smash(arr){
+   
+// }
+
+// console.log(smash(sentence));
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [5,3,8,1];
+
+// const filterRangeInPlace = (arr, a , b) => {
+  
+// }
+
+// filterRangeInPlace(arr, 1,4);
+
+// console.log(arr); // [3,1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let boolean = true;
+
+// const convertToBoolean = 
+
+// console.log(convertToBoolean(boolean)); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [5,3,8,1];
+
+// let filterRange = 
+
+// let filtered = filterRange(arr, 1,4);
+
+// console.log(filtered); //[3, 1]
+// console.log(arr); //[5, 3, 8, 1]
+
+
+
+
+
+
+
+
+
+
+
+
+// let john = { name: "John", surname: "Smith", id: 1};
+// let pete = { name: "Pete", surname: "Hunt", id: 2};
+// let mary = { name: "Mary", surname: "Key", id: 3};
+
+// let users = [john, pete, mary];
+
+// let usersMapped = 
+
+// console.log(usersMapped[0].id); // 1
+// console.log(usersMapped[0].fullName); // John Smith
+
+
+
+
+
+
+
+
+
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// const sortByAge = 
+
+// sortByAge(arr);
+
+// console.log(arr[0].name); //John
+// console.log(arr[1].name); //Mary
+// console.log(arr[2].name); //Pete
+
+
+
+
+
+
+
+
+
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = 
+
+// console.log(names); // ['John', 'Pete', 'Mary']
+
+
+
+
+
+
+
+
+// let number = 35231;
+
+// const reverseNum = 
+
+// console.log(reverseNum(number)); // [1, 3, 2, 5, 3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,3,4];
+
+// const shuffle = arr => {
+  
+// }
+
+// console.log(shuffle(arr));
+// console.log(shuffle(arr));
+// console.log(shuffle(arr));
+// console.log(shuffle(arr));
+
+
+
+
+
+
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ john, pete, mary ];
+
+// const getAverageAge = 
+
+// console.log(getAverageAge(arr)); // 27.6667
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function yesOrNo(){
+  
+
+// }
+
+// yesOrNo();
+
+
+
+
+
+
+
+
+
+// function unique(arr) {
+  
+// }
+
+// let strings = ['Hare', 'Krishna', 'Hare', 'Krishna', 'Krishna', 'Krishna', 'Hare', 'Hare', ':-O'];
+
+// console.log(unique(strings)); // Hare, Krishna, :-O
+
+
+
+
+
+
+
+
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20 },
+//   {id: 'ann', name: "Ann Smith", age: 24 },
+//   {id: 'pete', name: "Pete Peterson", age: 31 },
+// ];
+
+// const groupByID
+
+// let usersById = groupById(users);
+
+// console.log(usersById);
+
+
+
+
+
+
+
+// let string = 'Yellow';
+
+// const removeChar = 
+
+// console.log(removeChar(string));
+
+
+
+
+
+
+
+
+
+// let string = "The greatest victory is that which requires no battle";
+
+// const reverseWords = 
+
+// console.log(reverseWords(string));
+
+
+
+
+
+
+
+
+
+
+// let string = ["A", "B", "C"];
+
+
+// console.log(reverseArray(string));
+
+
+
+
+
+
+
+
+
+// let scores = ["3:1", "2:2", "0:1", "4:1", "2:0", "3:1", "2:2", "0:1", "4:1", "2:0"];
+
+// function teamScore(games){
+  
+// }
+
+// console.log(teamScore(scores)); // 20
+
+
+
+
+
+
+
+
+
+// let num = 5;
+
+
+
+// console.log(evenOrOdd(num));
+
+
+
+
+
+
+
+
+
+// let string = '5EN10R C1T1ZEN5 1 L0VE';
+
+
+
+// console.log(correct(string));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = 'Anna';
+
+// function reverseString(str){
+  
+// }
+
+// console.log(reverseString(string));
+
+// function checkPalindrome(str){
+    
+// }
+
+// console.log(checkPalindrome(string)); // Anna is a palindrome.
+
+
+
+
+
+
+
+
+
+
+// const movies = ['Princess Bride', 'The Fellowship of the Ring', 'Hidden Figures', 'Sea Beast'];
+
+
+
+
+
+
+
+
+
+
+// let string = 'Yellow';
+
+
+
+// console.log(removeChar(string));
+
+
+
+
+
+
+
+
+
+
+
+// const bobsFollowers = ['Summer', 'Autumn', 'Winter', 'Spring'];
+// const tinasFollowers = ['Summer', 'Autumn', 'Joy'];
+// let mutualFollowers = [];
+
+
+
+// console.log(mutualFollowers);
+
+
+
+
+
+
+
+
+// const bonusTime = 
+
+// console.log(bonusTime(100000, true)); // £1000000
+
+
+
+
+
+
+
+// let bestColors = ['green', 'blue', 'red', 'purple', 'black'];
+
+
+
+
+
+
+
+
+
+// let DNA = "GCAT";
+
+// const convertDNA = 
+
+// console.log(convertDNA(DNA));
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [5,2,1,-10,8];
+
+// const decreasingOrder = 
+
+// console.log(decreasingOrder(arr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+// let friendsList = 
+
+// console.log(friendsList);
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = '92385174';
+
+
+
+// console.log(convert(string));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const name = "Rudolfo";
+
+// const playingBanjo = 
+
+// console.log(playingBanjo(name));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = ['Hello', 'there', 'my', 'friend'];
+
+// const stars = 
+
+// console.log(stars(array));
+
+
+
+
+
+
+
+
+
+
+
+
+// const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// for(let i = 0; i < rapperArray.length; i++){
+//     console.log(rapperArray[i]);
+  
+// }
+
+// console.log("And if you don't know, now you know.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sister = 'Bim Promsawad';
+
+// const swapNames = 
+
+// console.log(swapNames(sister)); // 'Promsawad Bim'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function absVal100Minus5Nums(n1,n2,n3,n4,n5){
+   
+// }
+
+// absVal100Minus5Nums(20,30,40,50,20); // 60
+
+
+
+
+
+
+
+
+// const name = 'Bim Promsawad';
+
+
+// console.log(initials(name));
+
+
+
+
+
+
+
+
+
+
+
+// let string = '-anna-banana';
+
+// function camelize(str){
+
+// }
+
+// console.log(camelize(string)); // annaBanana
+
+
+
+
+
+
+
+
+
+
+// let string = 'ronronear';
+
+// const capitalizeWord = 
+
+// console.log(capitalizeWord(string));
+
+
+
+
+
+
+
+
+
+//COPY AND PASTE
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log(`${i} is even`)
+//     }
+// }
+
+
+
+
+ 
+
+
+
+// const stringy = 
+
+// console.log(stringy(6)); // 101010
+
+
+
+
+
+
+
+
+// let arr = [42,54,65,87,0];
+
+// const min = 
+// const max = 
+
+// console.log(min(arr), max(arr)); // 0 87
+
+
+
+
+
+
+
+
+// let array = [10,20,30];
+
+// function closingTime(arr){
+  
+
+// }
+
+// closingTime(array);
+
+
+
+
+
+
+
+
+
+
+
+// function countBy(x,n){
+
+// }
+
+// console.log(countBy(2,5));
+
+
+
+
+
+
+
+
+
+
+
+
+// function rockPaperScissors(){
+//     let result = Math.random();
+
+//     if(result < .33){
+//         return 'rock'
+//     }else if(result < .66){
+//         return 'paper'
+//     }else{
+//         return 'scissors'
+//     }
+// }
+
+// function checkWinner(playerChoice){
+//     let botChoice = rockPaperScissors();
+
+//     if(((playerChoice === 'rock') && (botChoice === 'scissors')) || ((playerChoice === 'paper') && 
+//     (botChoice === 'rock')) || ((playerChoice === 'scissors') && (botChoice === 'paper'))){
+//         return 'You win!'
+//     }else if(playerChoice === botChoice){
+//         return 'You tie!'
+//     }else{
+//         return 'You lose :('
+//     }
+// }
+
+// function playGameXTimes(arr){
+    
+// }
+
+// playGameXTimes(['rock', 'paper', 'scissors']); // always different
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = '4927173';
+
+// const convertTo1sAnd0s = 
+
+// console.log(convertTo1sAnd0s(string)); // 0101010
+
+
+
+
+
+
+
+
+
+
+
+// const greet = 
+
+// console.log(greet('Anna', 'Anna'));
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,5];
+
+
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+// let nums = [2,0,5,9,8];
+
+// const sumSecondHighAndLow = 
+
+// console.log(sumSecondHighAndLow(nums)); // 10
+
+
+
+
+
+
+
+
+
+
+
+
+// let sheep = [true, false, true, false, false, true, true, true]; //5
+
+// const countSheep =
+
+// console.log(countSheep(sheep)); // 5
+
+
+
+
+
+
+
+
+// function sheepMurmur(num){
+   
+// }
+
+// console.log(sheepMurmur(3));
+
+
+
+
+
+
+
+
+// function stringy(size){
+  
+// }
+
+// console.log(stringy(6));
+
+
+
+
+
+
+
+
+// function musketeers(){
+//     let musketeers = ['Athos', 'Porthos', 'Aramis'];
+
+    
+
+// }
+
+// musketeers();
+
+
+
+
+
+
+
+
+// let nums = [10,20,30];
+
+// const addThree = 
+
+// console.log(addThree(nums));
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,5];
+
+
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function multiplicationTable(num){
+  
+// }
+
+// console.log(multiplicationTable(5));
+
+
+
+
+
+
+
+
+
+
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// const copySorted = 
+
+// let sorted = copySorted(arr);
+
+// console.log(sorted); // CSS, HTML, JavaScript
+// console.log(arr); // HTML, JavaScript, CSS (no changes)
+
+
+
+
+
+
+// let string = '1234';
+// let string2 = '605';
+
+// const convertToNum = 
+
+// const convert = 
+
+// console.log(convertToNum(string), convert(string2)) // 1234 605
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const x = ['good', 'bad', 'good', 'good', 'bad', 'good'];
+
+// const well = 
+
+// console.log(well(x)); // I smell a series!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mouthSize = 
+
+// console.log(mouthSize("Alligator"));
+
+
+
+
+
+
+// function feast(beast, dish){
+    
+// }
+
+// console.log(feast('great blue heron', 'garlic naan'));
+
+
+
+
+
+
+
+
+
+
+
+// let string = 'Let your goal be a conjuring of who you want to be';
+
+// const removeSpaces = 
+
+// console.log(removeSpaces(string));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//COPY AND PASTE, THEN FINISH THE FUNCTION
+// function range(start, end){
+//     let array = [];
+
+//     for(let i = start; i <= end; i++){
+//         array.push(i);
+//     }
+
+//     return array;
+// }
+// console.log(range(1,10));
+
+
+
+
+
+
+
+
+
+
+// let array = [1, -4, 7, 12];
+
+// const positiveSum = 
+
+// console.log(positiveSum(array)); // 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1,2,2];
+
+// const squareSum = 
+
+// console.log(squareSum(array)); //9
+
+
+
+
+
+
+
+
+// function countChar(str, letter){
+ 
+// }
+
+// console.log(countChar('Hello', 'l')); // 2
+
+
+
+
+
+
+
+
+
+
+
+// let string = 'Calm joy is sustainable!';
+
+// function removeExclamationMarks(str){
+    
+// }
+
+// console.log(removeExclamationMarks(string));
+
+
+
+
+
+
+
+
+
+
+// let array = [1,2,3];
+
+
+
+// console.log(maps(array)); // [2, 4, 6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let junk = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
+
+// const findNeedle =
+
+// console.log(findNeedle(junk));
+
+
+
+
+
+
+
+
+
+
+
+
+// let integers = [1, '2', 3, '4', '5', 6];
+
+// const sumArrayValues = 
+
+// console.log(sumArrayValues(integers)); //21
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nums = [1, -2, 3, -4, 5];
+
+// const invert =
+
+// console.log(invert(nums)); // [-1, 2, -3, 4, -5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nums = [1,2,3,4,5,6];
+
+// function findAverage(array) {
+   
+// }
+
+// console.log(findAverage(nums)) // 3.5
+
+
+
+
+
+
+
+
+
+
+
+                                            
+
+
+
+// let array = [1, 2, 3, 4];
+
+// const grow = 
+
+// console.log(grow(array)); //24
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+
+// function countPositivesSumNegatives(input){
+  
+// }
+
+// console.log(countPositivesSumNegatives(array)); // [10, -65]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function pow(x,n){
+    
+// }
+
+// console.log(pow(3,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arrA = [10,10,10];
+// let arrB = [1,1,1];
+
+// const arrayPlusArray = 
+
+// console.log(arrayPlusArray(arrA,arrB)); // 33
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function countByFive(num){
+    
+// }
+
+// countByFive(16);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = ["Keep", "Remove", "Keep", "Remove", "Keep"];
+
+// function removeEveryOther(arr) {
+    
+// }
+
+// console.log(removeEveryOther(arr)); // ["Keep", "Keep", "Keep"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let letsDance = ["I", "study", "JavaScript", "right", "now"];
+
+
+
+// console.log(letsDance);
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,3,4,6,7,8];
+
+// function firstNonConsecutive(arr){
+   
+// }
+
+// console.log(firstNonConsecutive(arr)); //6
+
+
+
+
+
+
+
+
+
+
+
+// function howMuchILoveYou(nbPetals) {
+  
+// }
+
+// console.log(howMuchILoveYou(18)); // not at all
+
+
+
+
+
+
+
+
+
+
+
+
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+
+// const onlyEvens = 
+
+// console.log(onlyEvens(nums)); // [2, 4, 6, 8, 10]
+
+
+
+
+
+
+
+// let users = "Bilbo, Gandalf, Nazgul";
+
+// const strToArr = 
+
+// console.log(strToArr(users));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let career = "I'm a jr. dev.";
+
+// console.log(career);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const shortLongShort = 
+
+// console.log(shortLongShort("22","1")); // 1221
+// console.log(shortLongShort("1","22")); // 1221
+
+
+
+
+
+
+
+
+// let array = ['You', 'are', 'doing', 'great', 'yes', 'you', 'are']; // findIndex
+
+
+
+
+
+
+
+
+
+
+// let meerkat = ['tail', 'body', 'head'];
+
+// function fixTheMeerkat(arr){
+  
+// }
+
+// console.log(fixTheMeerkat(meerkat)); // ['head', 'body', 'tail']
+
+
+
+
+
+
+
+
+
+
+
+
+// function findMultiples(integer, limit){
+   
+// }
+
+// console.log(findMultiples(2,6)); // [2, 4, 6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let birds = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"];
+
+// function gooseFilter(birds) {
+//     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    
+// }
+
+// console.log(gooseFilter(birds)); // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const values = [3,11,7,2,9,10];
+
+// const arrayMax = 
+
+// console.log(arrayMax(values)); // 11
+
+
+
+
+
+
+
+
+
+
+
+// let arrayValue = [1, 2, 3, 4, 5];
+
+// function reverseArrayInPlace(arr) {
+   
+// }
+
+// console.log(reverseArrayInPlace(arrayValue)); // → [5, 4, 3, 2, 1]
+
+// console.log(arrayValue); // → [5, 4, 3, 2, 1]
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr1 = [1, 3, 5, 7, 9, 11, 12];
+// let arr2 = [1, 2, 3, 4, 5, 10, 12];
+
+// function mergArrays(arr1,arr2){
+   
+// }
+
+// console.log(mergArrays(arr1, arr2)); //[1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function nMultiplesOfX(x,n){
+  
+// }
+
+// console.log(nMultiplesOfX(2,5)); // [2,4,6,8,10]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1, 2, 1, 1, 3, 2];
+
+// const distinct = 
+
+// console.log(distinct(array)); // [1, 2, 3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let animals = ["sheep", "sheep", "sheep", "wolf", "sheep", "sheep", "sheep"];
+
+// function warnTheSheep(arr){
+   
+    
+// }
+
+// console.log(warnTheSheep(animals));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1,2,3];
+
+// function arrayToList(arr){
+ 
+// }
+
+// console.log(arrayToList(array));
+
+
+
+
+
+
+
+
+
+
+// let arr = [1,2,5];
+
+
+
+// console.log(arr); // use negative indexes
+
+
+
+
+
+
+
+
+
+
+
+
+// let list1 = {
+//     value: 1,
+//     rest: {
+//       value: 2,
+//       rest: {
+//         value: 3,
+//         rest: null
+//       }
+//     }
+// };
+
+// function listToArray(list){
+  
+// }
+
+// console.log(listToArray(list1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = 'Hello World';
+
+// function repeatChar(str){
+
+// }
+
+// console.log(repeatChar(string)); // HHeelllloo  WWoorrlldd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function yesOrNo(){
+ 
+// }
+
+// yesOrNo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [4,3,9,7,2,1];
+
+// function squareOrSquareRoot(array){
+    
+// }
+
+// console.log(squareOrSquareRoot(arr)); // [2,9,3,49,4,1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let list = {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null
+//     }
+// };
+
+
+// function prepend(element, list) {
+    
+// }
+  
+// let newList = prepend(1, list);
+// console.log(newList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let list2 = {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null
+//     }
+// };
+  
+// function nth(num, list){
+   
+// }
+  
+// console.log(nth(1, list2)); // 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function deepEqual(a, b){
+
+// }
+ 
+ 
+// let obj = {here: {is: "an"}, object: 2};
+ 
+// console.log(deepEqual(obj, obj));// → true
+// console.log(deepEqual(obj, {here: 1, object: 2}));// → false
+// console.log(deepEqual(obj, {here: {is: "an"}, object: 2})); // → true
+
+
+
+
+
+
+
+
+
+
+
+// function sumOfValues(){
+   
+// }
+
+// console.log(sumOfValues()); // 42
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [2, 1, 10];
+
+// function sumOfDifferences(arr){
+    
+// }
+
+// console.log(sumOfDifferences(array)); // 9
+
+
+
+
+
+
+
+
+
+
+
+// function followingDay(){
+//     let day = prompt('Enter a day of the week to get the following day:').toLowerCase();
+
+
+//     if(day === 'monday'){
+//         alert('Tuesday');
+//     }else if(day === 'tuesday'){
+//         alert('Wednesday')
+//     }else if(day === 'wednesday'){
+//         alert('Thursday')
+//     }else if(day === 'thursday'){
+//         alert('Friday')
+//     }else if(day === 'friday'){
+//         alert('Saturday')
+//     }else if(day === 'saturday'){
+//         alert('Sunday')
+//     }else if(day === 'sunday'){
+//         alert('Monday')
+//     }
+// }
+
+// followingDay();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = "A1, B2";
+
+// function removeFirstAndLast(str){
+    
+// }
+
+// console.log(removeFirstAndLast(string)); //null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [0,2,3,6,9];
+
+// function multipleOfIndex(arr){
+   
+// }
+
+// console.log(multipleOfIndex(array)); // [0, 2, 6]
+
+
+
+
+
+
+
+
+
+// let familyAges = [82, 15, 6, 38, 35];
+
+// function differenceInAges(ages){
+    
+// }
+
+// console.log(differenceInAges(familyAges));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
