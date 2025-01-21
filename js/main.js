a6b72f550bdb692d15d8fa6dace08e1f658a3a13
Anna Burnett <annairishburnett@gmail.com>
2025-01-21 17:28:43 -0500

    added new API question to Anki deck

--- js/main.js
+++ js/main.js
@@ -2158,3 +2158,28 @@
 // }
 
 // console.log(disemvowel(string)); //Ths wbst s s fnny LL!
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//         console.log(data.drinks[0]);
+//         document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+//         document.querySelector('h2').innerText = data.drinks[0].strDrink;
+//         document.querySelector('p').innerText = data.drinks[0].strInstructions;
+
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     });
