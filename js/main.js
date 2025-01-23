db2d720b98f7c7df0d7f3cdc72360ca2b5f644d3
Anna Burnett <annairishburnett@gmail.com>
2025-01-23 15:52:42 -0500

    added eloquent javascript task question

--- js/main.js
+++ js/main.js
@@ -2280,3 +2280,20 @@
 
 
 
+
+
+
+
+
+
+
+
+// function isEven(num){
+   
+
+// }
+  
+  
+// console.log(isEven(50));// → true
+// console.log(isEven(75));// → false
+// console.log(isEven(-1));// → ??
