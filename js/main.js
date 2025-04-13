61506d2594853b01c548356c8cb8b3caf0df1d75
Anna Burnett <annairishburnett@gmail.com>
2025-04-12 20:57:38 -0400

    solved anki problems of the day

--- js/main.js
+++ js/main.js
@@ -1440,20 +1440,13 @@
 
 
 
-let array = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15];
+// let array = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15];
 
-function countPositivesSumNegatives(input){
- const posCount = input.filter(num => num > 0).length;
- const negSum = input.reduce((acc, c) => acc + (c < 0 ? c : 0), 0);
+// function countPositivesSumNegatives(input){
+ 
+// }
 
- if(input === null || input.length < 1){
-        return [];
- }else{
-        return [posCount, negSum];
- }
-}
-
-console.log(countPositivesSumNegatives(array)); // [10, -65]
+// console.log(countPositivesSumNegatives(array)); // [10, -65]
 
 
 
