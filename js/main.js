e2bfcbb89c41aed8f34ef7159891ab5c9a1d83c7
Anna Burnett <annairishburnett@gmail.com>
2025-04-16 12:00:24 -0400

    added new 6-kyu codewars kata to anki deck

--- js/main.js
+++ js/main.js
@@ -3219,3 +3219,32 @@
 // }
 
 // console.log(likes(array));// "Evi, Lea and 3 others like this"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// function createPhoneNumber(nums){
+    
+// }
+
+// console.log(createPhoneNumber(number));// "(123) 456-7890"
+
