d308406ec9874afe6e426e39097e589e3d8b26e0
Anna Burnett <annairishburnett@gmail.com>
2025-06-24 11:09:16 -0400

    solved anki problems of the day and added new anki card

--- js/main.js
+++ js/main.js
@@ -684,9 +684,9 @@
 // const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
 
 // for(let i = 0; i < rapperArray.length; i++){
-    
+  
    
-    
+
   
 // }
 
@@ -2442,6 +2442,7 @@
 
 
 
+
 //Copy and paste section below, don't edit
 // fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 //     .then(res => res.json()) // parse response as JSON
@@ -2664,7 +2665,28 @@
 
 
 
+//add "Enter" functionality
+document.querySelector('#getCocktail').addEventListener('click', getDrink);
 
+
+function getDrink(){
+
+    const drink = document.querySelector('input').value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.drinks[0]);
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+        document.querySelector('h2').innerText = data.drinks[0].strDrink;
+        document.querySelector('p').innerText = data.drinks[0].strInstructions;
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+}
 
 
 
@@ -3109,8 +3131,6 @@
 
 
 
-
-
 // const string = "He11o th3r3 y0u!";
 
 // function filterString(value){
@@ -3526,10 +3546,10 @@
 
 
 
-// let string = "recede";
+// let string = "rEcede";
 
 // function duplicateEncode(word){
-  
+   
   
 // }
 
